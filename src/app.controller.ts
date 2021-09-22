cbf31c28e8e9d3e7e771e0cb73f7bc127a7ebd79
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-09-22 19:51:22 -0300

    ajustes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
   @Get('/api/public/aluno/')
   async getAluno(@Query() query: any) {
     const { data } = await axios.get(
-      `http://165.227.182.73:8000/api/public/aluno/?dsc_matricula=${query.dsc_matricula}&format=json`,
+      `http://165.227.182.73/api/public/aluno/?dsc_matricula=${query.dsc_matricula}&format=json`,
     );
     console.log(data);
     return data;
@@ -18,7 +18,7 @@ export class AppController {
   @Get('/api/public/conteudo/')
   async conteudos(@Query() query: any) {
     const { data } = await axios.get(
-      `http://165.227.182.73:8000/api/public/conteudo/?ano_letivo=2021&disciplina=${query.disciplina}`,
+      `http://165.227.182.73/api/public/conteudo/?ano_letivo=2021&disciplina=${query.disciplina}`,
     );
     console.log(data);
     return data;
@@ -27,7 +27,7 @@ export class AppController {
   @Get('/api/public/questao/')
   async questoes(@Query() query: any) {
     const { data } = await axios.get(
-      `http://165.227.182.73:8000/api/public/questao/?ano_letivo=2021&conteudo=${query.conteudo}`,
+      `http://165.227.182.73/api/public/questao/?ano_letivo=2021&conteudo=${query.conteudo}`,
     );
     console.log(data);
     return data;
@@ -36,7 +36,7 @@ export class AppController {
   @Get('/api/public/disciplina/')
   async disciplinas() {
     const { data } = await axios.get(
-      `http://165.227.182.73:8000/api/public/disciplina/?ano_letivo=2021`,
+      `http://165.227.182.73/api/public/disciplina/?ano_letivo=2021`,
     );
     return data;
   }
@@ -44,7 +44,7 @@ export class AppController {
   @Get('/api/public/cti/')
   async cti(@Query() query: any) {
     const { data } = await axios.get(
-      `http://165.227.182.73:8000/api/public/cti/?dsc_cti=&dat_cti=${query.dat_cti}&criador=&ano_letivo=2021`,
+      `http://165.227.182.73/api/public/cti/?dsc_cti=&dat_cti=${query.dat_cti}&criador=&ano_letivo=2021`,
     );
     return data;
   }
@@ -53,7 +53,7 @@ export class AppController {
   async saveDuvida(@Body() params: any) {
     console.log(33333333);
     const { data } = await axios.post(
-      `http://165.227.182.73:8000/api/public/duvida/`,
+      `http://165.227.182.73/api/public/duvida/`,
       params,
     );
     return data;
@@ -63,7 +63,7 @@ export class AppController {
   async saveCti(@Body() params: any) {
     console.log(params);
     const { data } = await axios.post(
-      `http://165.227.182.73:8000/api/public/cti/`,
+      `http://165.227.182.73/api/public/cti/`,
       params,
     );
     return data;
@@ -73,7 +73,7 @@ export class AppController {
   async saveCtiRemoto(@Body() params: any) {
     try {
       const { data } = await axios.post(
-        `http://165.227.182.73:8000/api/public/remoto/cti/`,
+        `http://165.227.182.73/api/public/remoto/cti/`,
         params,
       );
       return data;
